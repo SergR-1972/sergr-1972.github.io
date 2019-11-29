541e50b9155e57ae0ef0731787f15157a18dc57d
SergR-1972 <58327016+SergR-1972@users.noreply.github.com>
2019-11-29 11:20:04 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+/*let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello, world! / Привіт, світ!';*/
+
+let myImage = document.querySelector('img');
+myImage.onclick = function() {
+   let mySrc = myImage.getAttribute('src');
+/*   alert('mySrc='+ mySrc);*/
+   if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src', 'images/boxes.jpg');
+   } else {
+      myImage.setAttribute('src', 'images/firefox-icon.png');
+   }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName(){
+   let myName = prompt("Please, enter your name / Будь ласка, введіть своє ім'я");
+      if(!myName || myName === null){
+         setUserName();
+      }else{
+         localStorage.setItem('name', myName);
+         myHeading.textContent = 'Welcome / Добридень, ' + myName + '!';
+      }
+}
+
+if(!localStorage.getItem('name')){
+   setUserName();
+}else{
+   let storedName = localStorage.getItem('name');
+   myHeading.textContent = 'Hello(Привіт), ' + storedName;
+}
+
+myButton.onclick = function() {
+   setUserName();
+}
